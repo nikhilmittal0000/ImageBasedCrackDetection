a8a2a4366d1e81262007b7c86b5271f7d376bc95
nikhilmittal0000 <nikhilmittal0000@gmail.com>
2024-03-08 20:24:05 -0500

    Delete Morphological Operations directory

--- Morphological Operations/getCoordinates.m
+++
@@ -1,7 +0,0 @@
-function coor = getCoordinates(idx, imsize)
-    if rem(idx,imsize(1)) == 0
-        coor = [imsize(1), floor(idx/imsize(1))];
-    else
-        coor = [rem(idx,imsize(1)), floor(idx/imsize(1))+1];
-    end
-end
--- Morphological Operations/getIndex.m
+++
@@ -1,3 +0,0 @@
-function idx = getIndex(coor, imsize)
-    idx = (coor(2)-1)*imsize(1)+ coor(1);
-end
